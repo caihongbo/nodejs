64f4f5c5311843fe25b87762343d8d0db2a25d56
darlinglele <darlinglele@live.com>
2012-04-24 17:19:20 +0800

    add mime type

--- helper.js
+++ helper.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 function readFile(realPath, response){
+	var extension = path.extname(realPath);
 	path.exists(realPath, function (exists) {
 		if (!exists) {
 			response.writeHead(404, {'Content-Type': 'text/plain'});
@@ -10,9 +11,11 @@ function readFile(realPath, response){
 			fs.readFile(realPath, "binary", function(err, file) {
 				if (err) {
 					response.writeHead(500, {'Content-Type': 'text/plain'});
-					response.end(err);
+					response.end("error message");
 				} else {
-					response.writeHead(200, {'Content-Type': 'text/html'});
+
+console.log(contentTypes[extension]);
+					response.writeHead(200, {'Content-Type': contentTypes[extension]});
 					response.write(file, "binary");
 					response.end();
 				}
@@ -21,4 +24,55 @@ function readFile(realPath, response){
 	});
 }
 
+var contentTypes = {
+  ".aiff": "audio/x-aiff",
+  ".arj": "application/x-arj-compressed",
+  ".asf": "video/x-ms-asf",
+  ".asx": "video/x-ms-asx",
+  ".au": "audio/ulaw",
+  ".avi": "video/x-msvideo",
+  ".bcpio": "application/x-bcpio",
+  ".ccad": "application/clariscad",
+  ".cod": "application/vnd.rim.cod",
+  ".com": "application/x-msdos-program",
+  ".cpio": "application/x-cpio",
+  ".cpt": "application/mac-compactpro",
+  ".csh": "application/x-csh",
+  ".css": "text/css",
+  ".deb": "application/x-debian-package",
+  ".dl": "video/dl",
+  ".doc": "application/msword",
+  ".drw": "application/drafting",
+  ".dvi": "application/x-dvi",
+  ".dwg": "application/acad",
+  ".dxf": "application/dxf",
+  ".dxr": "application/x-director",
+  ".etx": "text/x-setext",
+  ".ez": "application/andrew-inset",
+  ".fli": "video/x-fli",
+  ".flv": "video/x-flv",
+  ".gif": "image/gif",
+  ".gl": "video/gl",
+  ".gtar": "application/x-gtar",
+  ".gz": "application/x-gzip",
+  ".hdf": "application/x-hdf",
+  ".hqx": "application/mac-binhex40",
+  ".html": "text/html",
+  ".ice": "x-conference/x-cooltalk",
+  ".ief": "image/ief",
+  ".igs": "model/iges",
+  ".ips": "application/x-ipscript",
+  ".ipx": "application/x-ipix",
+  ".jad": "text/vnd.sun.j2me.app-descriptor",
+  ".jar": "application/java-archive",
+  ".jpeg": "image/jpeg",
+  ".jpg": "image/jpeg",
+  ".js": "text/javascript",
+  ".json": "application/json",
+  ".latex": "application/x-latex",
+  ".lsp": "application/x-lisp",
+  ".lzh": "application/octet-stream",
+  ".m": "text/plain",
+  ".m3u": "audio/x-mpegurl"
+  };
 exports.write = readFile;  
